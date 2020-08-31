0d6b6257baaff4d87965f7b423c52444b32ab40c
Marco Tedone <marco@techwings.io>
2020-08-31 21:11:09 +0100

    Fix modal issue

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
-
-  constructor() { }
+export class ModalComponent implements OnInit, OnDestroy {
+  constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {
+    // To fix the Modal issue, which is that if the modal
+    // is displayed within an element with position:relative
+    // we need to append it to the body element of the AppComponent
+    document.body.appendChild(this.elementRef.nativeElement);
   }
 
+  ngOnDestroy(): void {
+    document.body.removeChild(this.elementRef.nativeElement);
+  }
 }
--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { ModalComponent } from './modal/modal.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ModsHomeComponent, ModalComponent],
-  imports: [
-    CommonModule,
-    ModsRoutingModule
-  ]
+  imports: [CommonModule, SharedModule, ModsRoutingModule],
 })
-export class ModsModule { }
+export class ModsModule {}
